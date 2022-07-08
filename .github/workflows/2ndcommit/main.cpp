ab4bca65d9c7c257318f892489aff2c5286df69c
kitb1357 <ccobi2003@gmail.com>
2022-07-08 10:44:40 -0600

    yes

---
+++ .github/workflows/2ndcommit/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter two numbers" << std::endl;
+    double a,b;
+    std::cin << a << b << std::endl;
+
+    std::cout << "Two numbers you've entered are : " << a << b << std::endl;
+    return 0;
+
+}
